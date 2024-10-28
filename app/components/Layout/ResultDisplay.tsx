0fb4103daad61e5220b09e2dccb4e6a5b6bcefec
oshimmmm <yasuhirooshima0419@gmail.com>
2024-10-28 18:13:01 +0900

    ResultDisplayコンポーネントをインラインスタイルへ修正

--- app/components/Layout/ResultDisplay.tsx
+++ app/components/Layout/ResultDisplay.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styles from '../../../app/style/style.module.css';
 
 interface MatchedContent {
   content: string[];
@@ -11,17 +10,57 @@ interface ResultDisplayProps {
 
 const ResultDisplay: React.FC<ResultDisplayProps> = ({ apiResult }) => {
   return (
-    <div className={styles.resultContainer}>
+    <div 
+      style={{
+        margin: '0px 0',
+        alignItems: 'flex-start',
+        minHeight: '50vh',
+      }}
+    >
+      <h2 style={{ fontWeight: 'bold', backgroundColor: '#f0e68c', padding: '10px', borderRadius: '5px' }}>肺判定</h2>
       {apiResult ? (
-        <ul className={styles.resultList}>
+        <ul style={{ listStyleType: 'none', padding: 0}}>
           {apiResult.map((matchedItem, index) => (
-            <li key={index} className={styles.resultItem}>
-              <div className={styles.matchedContentContainer}>
+            <li 
+              key={index} 
+              style={{
+                margin: '12px 0',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center',
+              }}
+            >
+              <div 
+                style={{
+                  marginBottom: '1.5em',
+                  padding: '1em',
+                  backgroundColor: '#f9f9f9',
+                  borderRadius: '8px',
+                }}
+              >
                 {matchedItem.content.map((content, contentIndex) => (
-                  <p key={contentIndex} className={styles.preWrapText}>{content}</p>
+                  <p 
+                    key={contentIndex} 
+                    style={{
+                      whiteSpace: 'pre-wrap',
+                      lineHeight: 1.2,
+                      marginBottom: '-0.5em',
+                    }}
+                  >
+                    {content}
+                  </p>
                 ))}
               </div>
-              {index < apiResult.length - 1 && <hr className={styles.divider} />}
+              {index < apiResult.length - 1 && 
+                <hr
+                  style={{
+                    margin: '2em 0',
+                    border: 'none',
+                    borderTop: '2px solid #ccc',
+                  }}
+               />}
             </li>
           ))}
         </ul>
